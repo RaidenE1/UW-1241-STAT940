3171195346cafb6a478075922155d4f20955a5fb
root <jhz.travis@outlook.com>
2024-03-07 01:30:40 +0000

    it can run now

--- dc2/train_setfit.py
+++ dc2/train_setfit.py
@@ -4,16 +4,17 @@ from setfit import Trainer
 from utils.dataset import CustomDataset
 from datasets import load_dataset
 from utils.load_data import *
-import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def main():
     model = SetFitModel.from_pretrained("BAAI/bge-small-en-v1.5")
-    model.labels = [0,1,2]
+    model.labels = [1,2,3]
     args = TrainingArguments(
-        batch_size = 1,
-        num_epochs = 10
+        batch_size = 64,
+        num_epochs = 5,
+        sampling_strategy="undersampling"
     )
 
     train_dataset = load_dataset("./data")['train']
@@ -27,8 +28,18 @@ def main():
             "stars": "label"
         }
     )
-    print("yes")
     trainer.train()
+    test_data = load_test_labels("./test.csv")
+    IDs = list(test_data.keys())
+    IDs.sort()
+    texts = [
+        test_data[ID] for ID in IDs
+    ]
+    predicts = model.predict(texts)
+    df = pd.DataFrame({'ID': IDs, 'stars': predicts})
+    df.to_csv('./out/result.csv', index=False)
+
+    
 
 
 if __name__ == '__main__':
